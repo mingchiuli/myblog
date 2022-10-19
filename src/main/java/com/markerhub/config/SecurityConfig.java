e7c9b5fd7a8b827bf6be064e6e6742ff54ad01d1
Mingjiu Li <Mingjiu.Li@disney.com>
2022-10-20 01:22:57 +0800

    循环依赖

--- src/main/java/com/markerhub/config/SecurityConfig.java
+++ src/main/java/com/markerhub/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
@@ -85,12 +86,6 @@ public class SecurityConfig {
         this.jwtLogoutSuccessHandler = jwtLogoutSuccessHandler;
     }
 
-
-
-    @Bean
-    BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     private static final String[] URL_WHITELIST = {
             "/captcha",
