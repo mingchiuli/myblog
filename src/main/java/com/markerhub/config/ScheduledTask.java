dad53d3ca5b5475f68fa14df12c90c136619bf75
Mingjiu Li <Mingjiu.Li@disney.com>
2022-10-27 14:52:27 +0800

    分布式锁

--- src/main/java/com/markerhub/config/ScheduledTask.java
+++ src/main/java/com/markerhub/config/ScheduledTask.java
@@ -30,6 +30,8 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class ScheduledTask {
 
+    private static final String scheduledLock = "scheduledLock";
+
     ThreadPoolExecutor executor;
 
     @Autowired
@@ -56,96 +58,102 @@ public class ScheduledTask {
     @SneakyThrows
     @Scheduled(cron = "0 0 0/2 * * ?")
     public void configureTask() {
-        int[] years = blogService.searchYears();
+        //分布式锁
+        Boolean ifAbsent = redisTemplate.opsForValue().setIfAbsent(scheduledLock, 1, 120, TimeUnit.SECONDS);
 
-        CompletableFuture<Void> var1 = CompletableFuture.runAsync(() -> {
-            //detail和getBlogStatus接口
-            List<BlogEntity> blogs = blogService.list(new QueryWrapper<BlogEntity>().ne("status", 1));
-            blogs.forEach(blog -> {
-                StringBuilder builder = new StringBuilder();
+        if (Boolean.TRUE.equals(ifAbsent)) {
+            int[] years = blogService.searchYears();
 
-                builder.append("::");
-                builder.append(blog.getId());
+            CompletableFuture<Void> var1 = CompletableFuture.runAsync(() -> {
+                //detail和getBlogStatus接口
+                List<BlogEntity> blogs = blogService.list(new QueryWrapper<BlogEntity>().ne("status", 1));
+                blogs.forEach(blog -> {
+                    StringBuilder builder = new StringBuilder();
 
-                String contentPrefix = Const.HOT_BLOG + "::BlogServiceImpl::getBlogDetail" + builder;
-                String statusPrefix = Const.BLOG_STATUS + "::BlogController::getBlogStatus" + builder;
+                    builder.append("::");
+                    builder.append(blog.getId());
 
-
-                redisTemplate.opsForValue().set(contentPrefix, blog, ThreadLocalRandom.current().nextInt(120) + 1, TimeUnit.MINUTES);
-                redisTemplate.opsForValue().set(statusPrefix, Result.succ(blog.getStatus()), ThreadLocalRandom.current().nextInt(120) + 1, TimeUnit.MINUTES);
-
-            });
-
-            //bloomFilter
-            List<BlogEntity> blogIds = blogService.list(new QueryWrapper<BlogEntity>().select("id"));
-            blogIds.forEach(blogId -> {
-                redisTemplate.opsForValue().setBit(Const.BLOOM_FILTER_BLOG, blogId.getId(), true);
-            });
+                    String contentPrefix = Const.HOT_BLOG + "::BlogServiceImpl::getBlogDetail" + builder;
+                    String statusPrefix = Const.BLOG_STATUS + "::BlogController::getBlogStatus" + builder;
 
 
+                    redisTemplate.opsForValue().set(contentPrefix, blog, ThreadLocalRandom.current().nextInt(120) + 1, TimeUnit.MINUTES);
+                    redisTemplate.opsForValue().set(statusPrefix, Result.succ(blog.getStatus()), ThreadLocalRandom.current().nextInt(120) + 1, TimeUnit.MINUTES);
 
-        }, executor);
+                });
 
-        CompletableFuture<Void> var2 = CompletableFuture.runAsync(() -> {
-            //list接口
-            long count = blogService.count();
-            long totalPage = count % Const.PAGE_SIZE == 0 ? count / Const.PAGE_SIZE : count / Const.PAGE_SIZE + 1;
-
-            for (int i = 1; i <= totalPage; i++) {
-                Page<BlogEntity> page = new Page<>(i, Const.PAGE_SIZE);
-                page = blogService.page(page, new QueryWrapper<BlogEntity>().select("id", "title", "description", "link", "created").orderByDesc("created"));
-                String sb = "::" + i;
-                String pagesPrefix = Const.HOT_BLOGS + "::BlogController::list" + sb;
-                redisTemplate.opsForValue().set(pagesPrefix, Result.succ(page), ThreadLocalRandom.current().nextInt(120) + 1, TimeUnit.MINUTES);
                 //bloomFilter
-                redisTemplate.opsForValue().setBit(Const.BLOOM_FILTER_PAGE, i, true);
-            }
-        }, executor);
+                List<BlogEntity> blogIds = blogService.list(new QueryWrapper<BlogEntity>().select("id"));
+                blogIds.forEach(blogId -> {
+                    redisTemplate.opsForValue().setBit(Const.BLOOM_FILTER_BLOG, blogId.getId(), true);
+                });
 
-        CompletableFuture<Void> var3 = CompletableFuture.runAsync(() -> {
-            //getCountByYear接口
-            for (int year : years) {
-                Integer countYear = blogService.getYearCount(year);
-                String sb = "::" + year;
-                String yearCountPrefix = Const.HOT_BLOGS + "::BlogController::getCountByYear" + sb;
-                redisTemplate.opsForValue().set(yearCountPrefix, Result.succ(countYear), ThreadLocalRandom.current().nextInt(120) + 1, TimeUnit.MINUTES);
-            }
-        }, executor);
 
-        CompletableFuture<Void> var4 = CompletableFuture.runAsync(() -> {
-            //listByYear接口
-            for (int year : years) {
 
-                //当前年份的总页数
-                LocalDateTime start = LocalDateTime.of(year, 1, 1, 0, 0, 0);
-                LocalDateTime end = LocalDateTime.of(year, 12, 31, 23, 59, 59);
-                long pageNum = blogService.count(new QueryWrapper<BlogEntity>().between("created", start, end));
-                for (int i = 1; i <= pageNum; i++) {
-                    //每一页的缓存
-                    Page<BlogEntity> pageData = blogService.listByYear(i, year);
-                    String sb = "::" + i + "::" + year;
-                    String yearListPrefix = Const.HOT_BLOGS + "::BlogController::listByYear" + sb;
-                    redisTemplate.opsForValue().set(yearListPrefix, Result.succ(pageData), ThreadLocalRandom.current().nextInt(120) + 1, TimeUnit.MINUTES);
-                    //bloom过滤器
-                    redisTemplate.opsForValue().setBit(Const.BLOOM_FILTER_PAGE + year, i, true);
+            }, executor);
+
+            CompletableFuture<Void> var2 = CompletableFuture.runAsync(() -> {
+                //list接口
+                long count = blogService.count();
+                long totalPage = count % Const.PAGE_SIZE == 0 ? count / Const.PAGE_SIZE : count / Const.PAGE_SIZE + 1;
+
+                for (int i = 1; i <= totalPage; i++) {
+                    Page<BlogEntity> page = new Page<>(i, Const.PAGE_SIZE);
+                    page = blogService.page(page, new QueryWrapper<BlogEntity>().select("id", "title", "description", "link", "created").orderByDesc("created"));
+                    String sb = "::" + i;
+                    String pagesPrefix = Const.HOT_BLOGS + "::BlogController::list" + sb;
+                    redisTemplate.opsForValue().set(pagesPrefix, Result.succ(page), ThreadLocalRandom.current().nextInt(120) + 1, TimeUnit.MINUTES);
+                    //bloomFilter
+                    redisTemplate.opsForValue().setBit(Const.BLOOM_FILTER_PAGE, i, true);
                 }
-            }
-        }, executor);
+            }, executor);
+
+            CompletableFuture<Void> var3 = CompletableFuture.runAsync(() -> {
+                //getCountByYear接口
+                for (int year : years) {
+                    Integer countYear = blogService.getYearCount(year);
+                    String sb = "::" + year;
+                    String yearCountPrefix = Const.HOT_BLOGS + "::BlogController::getCountByYear" + sb;
+                    redisTemplate.opsForValue().set(yearCountPrefix, Result.succ(countYear), ThreadLocalRandom.current().nextInt(120) + 1, TimeUnit.MINUTES);
+                }
+            }, executor);
+
+            CompletableFuture<Void> var4 = CompletableFuture.runAsync(() -> {
+                //listByYear接口
+                for (int year : years) {
+
+                    //当前年份的总页数
+                    LocalDateTime start = LocalDateTime.of(year, 1, 1, 0, 0, 0);
+                    LocalDateTime end = LocalDateTime.of(year, 12, 31, 23, 59, 59);
+                    long pageNum = blogService.count(new QueryWrapper<BlogEntity>().between("created", start, end));
+                    for (int i = 1; i <= pageNum; i++) {
+                        //每一页的缓存
+                        Page<BlogEntity> pageData = blogService.listByYear(i, year);
+                        String sb = "::" + i + "::" + year;
+                        String yearListPrefix = Const.HOT_BLOGS + "::BlogController::listByYear" + sb;
+                        redisTemplate.opsForValue().set(yearListPrefix, Result.succ(pageData), ThreadLocalRandom.current().nextInt(120) + 1, TimeUnit.MINUTES);
+                        //bloom过滤器
+                        redisTemplate.opsForValue().setBit(Const.BLOOM_FILTER_PAGE + year, i, true);
+                    }
+                }
+            }, executor);
 
 
-        //searchYears和getCountByYear
-        CompletableFuture<Void> var5 = CompletableFuture.runAsync(() -> {
-            int[] ints = blogService.searchYears();
-            String yearKey = Const.YEARS + "::BlogController::searchYears";
-            redisTemplate.opsForValue().set(yearKey, Result.succ(ints), ThreadLocalRandom.current().nextInt(120) + 1, TimeUnit.MINUTES);
-            //getCountByYear的bloom
-            for (int year : ints) {
-                redisTemplate.opsForValue().setBit(Const.BLOOM_FILTER_YEARS, year, true);
-            }
-        }, executor);
+            //searchYears和getCountByYear
+            CompletableFuture<Void> var5 = CompletableFuture.runAsync(() -> {
+                int[] ints = blogService.searchYears();
+                String yearKey = Const.YEARS + "::BlogController::searchYears";
+                redisTemplate.opsForValue().set(yearKey, Result.succ(ints), ThreadLocalRandom.current().nextInt(120) + 1, TimeUnit.MINUTES);
+                //getCountByYear的bloom
+                for (int year : ints) {
+                    redisTemplate.opsForValue().setBit(Const.BLOOM_FILTER_YEARS, year, true);
+                }
+            }, executor);
 
-        CompletableFuture.allOf(var1, var2, var3, var4, var5).get();
+            CompletableFuture.allOf(var1, var2, var3, var4, var5).get();
 
-        log.info("定时任务执行完毕");
+            log.info("定时任务执行完毕");
+            redisTemplate.delete(scheduledLock);
+        }
     }
 }
